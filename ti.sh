28da76e20a2a73b64ddc3e2a2820229f602bb029
sneakyninjaMX <106525542+sneakyninjaMX@users.noreply.github.com>
2023-02-10 17:13:18 +0000

    internet test
    bash ti.sh
    will test your internet by pinging google.com
    will also show you the your ip and other useful information such as speed and packet loss

---
+++ ti.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo ip adress-
+echo ------------
+ip r
+echo ==============================================================================
+echo Pinging googel.com
+echo --------------------
+ping googel.com -c 3
